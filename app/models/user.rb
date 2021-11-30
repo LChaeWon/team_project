b547b9b307d914ff46762fb1183ed2db5b23f014
leehyean <leehyean001110@naver.com>
2021-11-30 07:42:16 +0000

    third

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+	
+  belongs_to :posts
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
+	
+	def is_like? (post)
+		Like.find_by(user_id: self.id, post_id: post.id).present?
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :chats
   resources :posts
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'posts#index'
+ 
+root 'posts#index'
+post 'post/:post_id/like' => 'likes#like_toggle'
+get 'users/sign_out'
 end
